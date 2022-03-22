38c6548eaad0289e06abac49a214087ee2e91775
WayraArendartchukCastro <wayraac@gmail.com>
2022-03-22 18:48:53 -0300

    campo de busca por nome e ordenação alfabetica

--- src/data.js
+++ src/data.js
@@ -11,4 +11,21 @@ export const filterFilms = (films, valueSelec) =>
   if (orderSelec === "Z-A"){
     return films.sort((a,b) => a.title > b.title ? -1 : 1)
   }
-}*/
+}
+*/
+
+//busca por nome
+export const searchCharacter = (character, characterSelected) =>
+character.filter(item => item.name.toUpperCase().includes(characterSelected.toUpperCase()));
+
+
+//ordem alfabética
+export const filterAlfa = (arrCharacters, orderSelec) => {
+  if (orderSelec === "a-z"){
+    return arrCharacters.sort((a,b) => a.name > b.name ? 1 : -1)
+  }
+
+  if (orderSelec === "z-a"){
+    return arrCharacters.sort((a,b) => a.name > b.name ? -1 : 1)
+  }
+}

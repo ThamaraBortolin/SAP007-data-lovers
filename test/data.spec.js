a8363b85b83ab110904d4ac113c361f87c14b241
WayraArendartchukCastro <wayraac@gmail.com>
2022-03-24 14:03:47 -0300

    feat/testes-personagens

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,71 @@
-import { example, anotherExample } from '../src/data.js';
+import {  searchCharacter,filterAlfa } from '../src/data.js';
 
+const objChacact = [
+  {
+    "name": "Taeko Okajima",
+  },
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+  {
+    "name": "Mr. Okajima",
+  },
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  {
+    "name": "Grandmother Okajima",
+  }
+]
+describe('test searchCharacter', () =>{
+  it('is a function', () =>{
+    expect(typeof searchCharacter).toBe('function')
+  })
+
+  it('returns searchCharacter', () => {
+    expect(searchCharacter(objChacact, "Taeko Okajima")).toEqual([
+      {
+        "name": "Taeko Okajima",
+      }
+    ]);
   });
 });
 
+describe('test filteAz', () =>{
+  it('is a function', () =>{
+    expect(typeof filterAlfa).toBe('function')
+  })
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+  it('returns filterAz', () => {
+    expect(filterAlfa(objChacact,"a-z")).toEqual([
+      {
+        "name": "Grandmother Okajima",
+      },
+
+      {
+        "name": "Mr. Okajima",
+      },
+
+      {"name": "Taeko Okajima",
+      }
+    ]);
   });
+});
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe('test filterZa', () =>{
+  it('is a function', () =>{
+    expect(typeof filterAlfa).toBe('function')
+  })
+
+  it('returns filterZa', () => {
+    expect(filterAlfa(objChacact,"z-a")).toEqual([
+      {
+        "name": "Taeko Okajima",
+      },
+
+      {
+        "name": "Mr. Okajima",
+      },
+
+      {
+        "name": "Grandmother Okajima",
+      }
+    ]);
   });
 });

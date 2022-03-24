54440fb67d9cdaab58848285e0eafefb0da2261e
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-03-24 14:16:07 -0300

    testes finalizados

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,217 @@
-import { example, anotherExample } from '../src/data.js';
+import { describe, it } from 'eslint/lib/rule-tester/rule-tester';
+import { filterFilms, filters, searchCharacter } from '../src/data.js';
 
+const objFilms = [
+  {
+    "title": "My Neighbor Totoro",
+    "release_date": "1988",
+    "rt_score": "93",
+  },
 
-describe('example', () => {
+  {
+    "title": "Kiki's Delivery Service",
+    "release_date": "1989",
+    "rt_score": "96",
+  },
+
+  {
+    "title": "Tales from Earthsea",
+    "release_date": "2006",
+    "rt_score": "41",
+  }
+]
+
+const objChacact = [
+  {
+    "name": "Taeko Okajima",
+  },
+
+  {
+    "name": "Mr. Okajima",
+  },
+
+  {
+    "name": "Grandmother Okajima",
+  }
+]
+
+describe('test filterFilms', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterFilms).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('return search films', () => {
+    expect(filterFilms(objFilms,"Tales from Earthsea")).toEqual([{
+      "title": "Tales from Earthsea",
+      "release_date": "2006",
+      "rt_score": "41",
+    }]);
   });
 });
 
 
-describe('anotherExample', () => {
+describe('test orderAToZ', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filters).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns order a-z', () => {
+    expect(filters(objFilms, "a-z")).toEqual([{
+      "title": "Kiki's Delivery Service",
+      "release_date": "1989",
+      "rt_score": "96",
+    },
+    {
+      "title": "My Neighbor Totoro",
+      "release_date": "1988",
+      "rt_score": "93",
+    },
+    {
+      "title": "Tales from Earthsea",
+      "release_date": "2006",
+      "rt_score": "41",
+    }]);
   });
 });
+
+describe('test orderZToA', () => {
+  it('is a function', () => {
+    expect(typeof filters).toBe('function');
+  });
+
+  it('returns order z-a', () => {
+    expect(filters(objFilms, "z-a")).toEqual([{
+      "title": "Tales from Earthsea",
+      "release_date": "2006",
+      "rt_score": "41",
+    },
+    {
+      "title": "My Neighbor Totoro",
+      "release_date": "1988",
+      "rt_score": "93",
+    },
+    {
+      "title": "Kiki's Delivery Service",
+      "release_date": "1989",
+      "rt_score": "96",
+    }
+  ]);
+  });
+});
+
+describe('test orderOldest', () =>{
+  it('is a function', () =>{
+    expect(typeof filters).toBe('function')
+  })
+
+  it('returns order oldest', () =>{
+    expect(filters(objFilms, "oldest")).toEqual([
+      {
+        "title": "My Neighbor Totoro",
+        "release_date": "1988",
+        "rt_score": "93",
+      },
+      {
+        "title": "Kiki's Delivery Service",
+        "release_date": "1989",
+        "rt_score": "96",
+      },
+      {
+        "title": "Tales from Earthsea",
+        "release_date": "2006",
+        "rt_score": "41",
+      },
+    ])
+  })
+})
+
+describe('test orderNewest', () =>{
+  it('is a function', () =>{
+    expect(typeof filters).toBe('function')
+  })
+
+  it('returns order newest', () =>{
+    expect(filters(objFilms, "newest")).toEqual([
+      {
+        "title": "Tales from Earthsea",
+        "release_date": "2006",
+        "rt_score": "41",
+      },
+      {
+        "title": "Kiki's Delivery Service",
+        "release_date": "1989",
+        "rt_score": "96",
+      },
+      {
+        "title": "My Neighbor Totoro",
+        "release_date": "1988",
+        "rt_score": "93",
+      }
+    ])
+  })
+})
+
+describe('test orderLower', () =>{
+  it('is a function', () =>{
+    expect(typeof filters).toBe('function')
+  })
+
+  it('resturns order lower', () =>{
+    expect(filters(objFilms, "lower")).toEqual([
+      {
+        "title": "Tales from Earthsea",
+        "release_date": "2006",
+        "rt_score": "41",
+      },
+      {
+        "title": "My Neighbor Totoro",
+        "release_date": "1988",
+        "rt_score": "93",
+      },
+      {
+        "title": "Kiki's Delivery Service",
+        "release_date": "1989",
+        "rt_score": "96",
+      }
+    ])
+  })
+})
+
+describe('test orderHigher', () =>{
+  it('is a function', () =>{
+    expect(typeof filters).toBe('function')
+  })
+
+  it('returns order higher', () => {
+    expect(filters(objFilms, "higher")).toEqual([
+      {
+        "title": "Kiki's Delivery Service",
+        "release_date": "1989",
+        "rt_score": "96",
+      },
+      {
+        "title": "My Neighbor Totoro",
+        "release_date": "1988",
+        "rt_score": "93",
+      },
+      {
+        "title": "Tales from Earthsea",
+        "release_date": "2006",
+        "rt_score": "41",
+      }
+    ])
+  })
+})
+
+describe('test searchCharacter', () =>{
+  it('is a function', () =>{
+    expect(typeof searchCharacter).toBe('function')
+  })
+
+  it('returns searchCharacter', () => {
+    let returnTest = [objChacact[0]]
+    expect(searchCharacter(objChacact, "Taeko Okajima")).toEqual(returnTest)
+  })
+})
+
+
